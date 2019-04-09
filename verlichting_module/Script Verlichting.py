cfd8dc1981c5d4fb501c81b4fa6b719375045720
Don Pet <pspdonny@live.com>
2019-04-09 18:55:01 +0200

    Verlichting
    Basis functionality + 3 statuses

---
+++ verlichting_module/Script Verlichting.py
@@ -0,0 +1,65 @@
+import time
+
+from phue import Bridge
+
+b = Bridge('192.168.0.101')
+b.connect()
+b.get_api()
+def all_on():
+        b.set_light(8,'on', True)
+        b.set_light(13,'on', True)
+        b.set_light(2,'on', True)
+        b.set_light(15,'on', True)
+        b.set_light(4,'on', True)
+
+def all_off():
+        b.set_light(8,'on', False)
+        b.set_light(13,'on', False)
+        b.set_light(2,'on', False)
+        b.set_light(15,'on', False)
+        b.set_light(4,'on', False)
+
+def all_brightness(brightness):
+        b.set_light(8, 'bri', brightness)
+        b.set_light(13, 'bri', brightness)
+        b.set_light(2, 'bri', brightness)
+        b.set_light(15, 'bri', brightness)
+        b.set_light(4, 'bri', brightness)
+
+def set_brightness(lamp, bright):
+        b.set_light(lamp, 'bri', bright)
+
+def set_on(lamp):
+        b.set_light(lamp, 'on', True)
+
+def set_off(lamp):
+        b.set_light(lamp, 'on', False)
+
+def set_status(status):
+        all_on()
+        if(status == 0):
+                b.set_light(8, 'bri', 0)
+                b.set_light(13, 'bri', 0)
+                b.set_light(2, 'bri', 0)
+                b.set_light(15, 'bri', 0)
+                b.set_light(4, 'bri', 0)
+
+        elif(status == 1):
+                b.set_light(8, 'bri', 255)
+                b.set_light(13, 'bri', 125)
+                b.set_light(2, 'bri', 0)
+                b.set_light(15, 'bri', 125)
+                b.set_light(4, 'bri', 255)
+
+        elif(status == 2):
+                b.set_light(8, 'bri', 255)
+                b.set_light(13, 'bri', 255)
+                b.set_light(2, 'bri', 255)
+                b.set_light(15, 'bri', 255)
+                b.set_light(4, 'bri', 255)
+
+set_status(0)
+time.sleep(2)
+set_status(1)
+time.sleep(2)
+set_status(2)
