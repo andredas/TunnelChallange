d4118d33f4bcea364b0b30c5865b983cbd06c601
Don Pet <pspdonny@live.com>
2019-04-10 14:18:53 +0200

    Beaglebone compatibility

--- verlichting_module/scriptVerlichting.py
+++ verlichting_module/scriptVerlichting.py
@@ -1,15 +1,13 @@
-import time
-
-#from phue import Bridge
+from phue import Bridge
 #
-#b = Bridge('192.168.0.45')
-# b.connect()
-# b.get_api()
+b = Bridge('192.168.0.45')
+b.connect()
+b.get_api()
 
 
-#def all_on():
-#         b.set_light([8,13,2,15,4],'on', True)
-#
+def all_on():
+         b.set_light([8,13,2,15,4],'on', True)
+
 #
 # def all_off():
 #         b.set_light([8,13,2,15,4],'on', False)
@@ -28,24 +26,24 @@ import time
 # def set_off(lamp):
 #         b.set_light(lamp, 'on', False)
 
-#def get_info(lamp, typeI):
-#    stringInfo = "'" + typeI + "'"
-#    info = b.get_light(lamp, ("'", typeI, "'"))
-#   return info
+def get_info(lamp, typeI):
+   stringInfo = "'" + typeI + "'"
+   info = b.get_light(lamp, ("'", typeI, "'"))
+   return info
 
 def set_status(status):
-#         all_on()
+        all_on()
         if(status == "0"):
-#                 b.set_light([8,13,2,15,4], 'bri', 0)
-#
-#         elif(status == "1"):
-#                 b.set_light([8,4], 'bri', 255)
-#                 b.set_light([13,15], 'bri', 125)
-#                 b.set_light(2, 'bri', 0)
-#
-#         elif(status == "2"):
-#                 b.set_light([8,13,2,15,4], 'bri', 255)
-            return 1
+                b.set_light([8,13,2,15,4], 'bri', 0)
+
+        elif(status == "1"):
+                b.set_light([8,4], 'bri', 255)
+                b.set_light([13,15], 'bri', 125)
+                b.set_light(2, 'bri', 0)
+
+        elif(status == "2"):
+                b.set_light([8,13,2,15,4], 'bri', 255)
+                return 1
 
         else:
             return 0
