56c8aa83f93071b70061772ae163ac19e22bb592
André Das <andre@staad-groep.nl>
2019-04-10 09:50:55 +0200

    Fix conflicts

--- mmi/src/app/pages/cctv/cctv.module.ts
+++ mmi/src/app/pages/cctv/cctv.module.ts
@@ -3,28 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgxEchartsModule } from 'ngx-echarts';
 
 import { ThemeModule } from '../../@theme/theme.module';
-import { DashboardComponent } from './dashboard.component';
-import { StatusCardComponent } from './status-card/status-card.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { RoomSelectorComponent } from './rooms/room-selector/room-selector.component';
-import { TemperatureComponent } from './temperature/temperature.component';
-import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
-import { TeamComponent } from './team/team.component';
-import { KittenComponent } from './kitten/kitten.component';
-import { SecurityCamerasComponent } from './security-cameras/security-cameras.component';
-import { ElectricityComponent } from './electricity/electricity.component';
-import { ElectricityChartComponent } from './electricity/electricity-chart/electricity-chart.component';
-import { WeatherComponent } from './weather/weather.component';
-import { SolarComponent } from './solar/solar.component';
-import { PlayerComponent } from './rooms/player/player.component';
-import { TrafficComponent } from './traffic/traffic.component';
-import { TrafficChartComponent } from './traffic/traffic-chart.component';
-import { NotificationCenterComponent } from './notification-center/notification-center.component';
-import { ControlPanelComponent } from './control-panel/control-panel.component';
-import { MapOverviewComponent } from './map-overview/map-overview.component';
-import { SystemOverviewComponent } from './system-overview/system-overview.component';
-import { DetailOverviewComponent } from './detail-overview/detail-overview.component';
+
 
 @NgModule({
   imports: [
@@ -32,28 +11,7 @@ import { DetailOverviewComponent } from './detail-overview/detail-overview.compo
     NgxEchartsModule,
   ],
   declarations: [
-    DashboardComponent,
-    StatusCardComponent,
-    TemperatureDraggerComponent,
-    ContactsComponent,
-    RoomSelectorComponent,
-    TemperatureComponent,
-    RoomsComponent,
-    TeamComponent,
-    KittenComponent,
-    SecurityCamerasComponent,
-    ElectricityComponent,
-    ElectricityChartComponent,
-    WeatherComponent,
-    PlayerComponent,
-    SolarComponent,
-    TrafficComponent,
-    TrafficChartComponent,
-    NotificationCenterComponent,
-    ControlPanelComponent,
-    MapOverviewComponent,
-    SystemOverviewComponent,
-    DetailOverviewComponent,
+
   ],
 })
 export class CctvModule { }
